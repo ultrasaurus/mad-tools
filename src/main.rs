ac718156da2351f2779cf1ef410b41a714447326
Sarah Allen <sarah@ultrasaurus.com>
2023-09-07 21:38:58 -0700

    fetch basic account info, display count

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
+pub struct Account {
+    pub id: String,
+    pub username: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://httpbin.org/ip")
+    let resp = reqwest::get("https://quakers.social/api/v1/directory?local=true")
         .await?
-        .json::<HashMap<String, String>>()
+        .json::<Vec<Account>>()
         .await?;
     println!("{:#?}", resp);
+    println!("count: {:#?}", resp.len());
     Ok(())
 }
